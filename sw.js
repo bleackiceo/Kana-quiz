8e3465c64a5f684a22c4656a0e575c906d0db92b
bleackiceo <104562062+bleackiceo@users.noreply.github.com>
2026-09-19 00:05:38 +0800

    Refresh cache after listening-mode polish

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kana-quiz-v2';
+const CACHE='kana-quiz-v3';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
