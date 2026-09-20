c13e46bc85ab092c291bfc00a02c76887981ab60
bleackiceo <104562062+bleackiceo@users.noreply.github.com>
2026-09-20 14:03:41 +0800

    Refresh offline cache for vocabulary layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='kana-quiz-offline-v3';
+const CACHE='kana-quiz-offline-v4';
 const ASSETS=['./','./index.html','./manifest.json','./icon.svg','./words.js'];
 
 self.addEventListener('install', event => {
